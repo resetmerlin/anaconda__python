88ffbb5eb8d448aae7af9cb0287979db5314857f
resetmerlin <108568153+resetmerlin@users.noreply.github.com>
2022-09-01 18:18:38 +0900

    2022/09/01

---
+++ calculator.py
@@ -0,0 +1,39 @@
+def get_yearly_revenue(monthly_revenue):
+    return monthly_revenue*12
+
+# Don't touch anthing below this line 🙅🏻‍♂️🙅🏻‍♀️
+
+
+def get_yearly_expenses(monthly_expenses):
+    return monthly_expenses*12
+
+
+revenue = get_yearly_revenue(5500000)
+expense = get_yearly_expenses(2700000)
+profit = revenue-expense
+
+
+def get_tax_amount(profit):
+    if profit > 100000:
+        return profit*0.25
+
+    else:
+        return profit*0.15
+
+
+def apply_tax_credits(tax_amount, tax_credits):
+    return tax_amount*tax_credits
+
+
+monthly_revenue = 5500000
+monthly_expenses = 2700000
+tax_credits = 0.01
+
+profit = get_yearly_revenue(monthly_revenue) - \
+    get_yearly_expenses(monthly_expenses)
+
+tax_amount = get_tax_amount(profit)
+
+final_tax_amount = tax_amount - apply_tax_credits(tax_amount, tax_credits)
+
+print(f"Your tax bill is: ${final_tax_amount}")
